24d2a159456ca597d0d2e24d0b1b3b4373690baf
최우석 <56614719+chldntjr1321@users.noreply.github.com>
2026-03-16 22:07:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -2,7 +2,7 @@ function solution(absolutes, signs) {
     let newNums = [];
     
     for(let i=0; i<absolutes.length; i++) {
-        newNums.push(signs[i] === false ? absolutes[i] *= -1 : absolutes[i]);
+        newNums.push(signs[i] ? absolutes[i] : absolutes[i] *= -1);
     }
     return newNums.reduce((acc, cur) => acc + cur);
 }
